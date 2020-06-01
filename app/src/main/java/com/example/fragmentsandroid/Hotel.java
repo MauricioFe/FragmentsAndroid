69443e8999de5aebd420ca9007d281fa06b7e551
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-06-01 17:19:25 -0300

    Implementação da classe Hotel

---
+++ app/src/main/java/com/example/fragmentsandroid/Hotel.java
@@ -0,0 +1,39 @@
+package com.example.fragmentsandroid;
+
+import java.io.Serializable;
+
+public class Hotel implements Serializable {
+    private String nome;
+    private String endereco;
+    private float estrelas;
+
+    public Hotel(String nome, String endereco, float estrelas) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.estrelas = estrelas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public float getEstrelas() {
+        return estrelas;
+    }
+
+    public void setEstrelas(float estrelas) {
+        this.estrelas = estrelas;
+    }
+}
