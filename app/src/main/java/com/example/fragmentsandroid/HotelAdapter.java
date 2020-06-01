b5c90747ef92e7873c867359622452c0cbd8ce76
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-06-01 18:29:11 -0300

    Update Adapter

--- app/src/main/java/com/example/fragmentsandroid/HotelAdapter.java
+++ app/src/main/java/com/example/fragmentsandroid/HotelAdapter.java
@@ -1,14 +1,22 @@
 package com.example.fragmentsandroid;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
 public class HotelAdapter extends BaseAdapter {
-    HotelListFragment listFragment = new HotelListFragment();
-    List<Hotel> mHotel = listFragment.mHoteis;
+    private final List<Hotel> mHotel;
+    private final Context context;
+
+    public HotelAdapter(List<Hotel> mHotel, Context context) {
+        this.mHotel = mHotel;
+        this.context = context;
+    }
 
     @Override
     public int getCount() {
@@ -27,6 +35,13 @@ public class HotelAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_hoteis, parent, false);
+        Hotel hotel = mHotel.get(position);
+
+        TextView nome = view.findViewById(R.id.itens_hotel_nome);
+        nome.setText(hotel.getNome());
+        TextView endereco = view.findViewById(R.id.itens_hotel_endereco);
+        endereco.setText(hotel.getEndereco());
         return null;
     }
 }
